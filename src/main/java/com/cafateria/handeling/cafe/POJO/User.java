4a8995598e8ec755fc66c7b6ba74dcb13bf03170
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-05-31 09:52:53 +0545

    updated not appceting serializable implementation

--- src/main/java/com/cafateria/handeling/cafe/POJO/User.java
+++ src/main/java/com/cafateria/handeling/cafe/POJO/User.java
@@ -6,12 +6,14 @@ import lombok.Generated;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+import java.io.Serializable;
+
 @Data       //because of this data we don't need to create getter & setter and constructor
 @Entity
 @DynamicInsert
 @DynamicUpdate
 @Table(name = "user ")
-public class User implements Serializable{
+public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
