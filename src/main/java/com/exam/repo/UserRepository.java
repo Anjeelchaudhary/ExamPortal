c0d5421bb7a15bc4175e7e44679247a10f389358
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-06-05 16:45:59 +0545

    added interfae for Repository

--- src/main/java/com/exam/repo/UserRepository.java
+++ src/main/java/com/exam/repo/UserRepository.java
@@ -1,2 +1,8 @@
-package com.exam.repo;public interface UserRepository {
+package com.exam.repo;
+
+import com.exam.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
 }
