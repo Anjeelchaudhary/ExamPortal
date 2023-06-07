1149bdf76812db420bfe45cc5f3833e8b9ae0bb9
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-06-07 19:08:53 +0545

    stuck at UserController

--- src/main/java/com/exam/controller/UserController.java
+++ src/main/java/com/exam/controller/UserController.java
@@ -1,14 +1,38 @@
 package com.exam.controller;
 
+import com.exam.models.Role;
 import com.exam.models.User;
+import com.exam.models.UserRole;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Controller
+//here this method of mapping reutrns all the users
 @RequestMapping("/user")
 public class UserController {
-    //creating User
-    public User createUser() {
 
+    @Autowired
+    private UserService userService;
+
+    Set<UserRole> roles = new HashSet<>();
+    Role role = new Role();
+    role.setRoleId(45L);
+    role.setRoleName("");
+
+    UserRole userRole = new UserRole();
+    userRole.setUser(user);
+
+    roles.add();
+    //creating User
+    @PostMapping("/")
+    public User createUser(@RequestBody User user) {
+        return this.userService.createUser(user,);
     }
 }
