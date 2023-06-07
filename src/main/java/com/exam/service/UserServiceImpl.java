04b9ea10fc3f4240547d898929c219b7a499e755
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-06-07 10:43:48 +0545

    setted roles of user

--- src/main/java/com/exam/service/UserServiceImpl.java
+++ src/main/java/com/exam/service/UserServiceImpl.java
@@ -21,13 +21,18 @@ public class UserServiceImpl implements UserService {
     public User createUser(User user, Set<UserRole> userRoles) throws Exception {
 
         //yeadi pailai same username vako db ma xa vani yesle alert pathaunexa
-        User local = this.userRepository.findByUserName(user.getUsername());
+        User local = this.userRepository.findByUsername(user.getUsername());
         if (local != null) {
             System.out.println("User is already there");
             throw new Exception("Already same person is available in database");
         }else {
-
+            //User
+            for (UserRole ur:userRoles) {
+                roleRepository.save(ur.getRole());
+            }
+            user.getUserRoles().addAll(userRoles);
+            local = this.userRepository.save(user);
         }
-        return null;
+        return local;
     }
 }
